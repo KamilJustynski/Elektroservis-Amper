f499991c325415ab3650668e4137437ea81343f9
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-11-14 19:14:14 +0100

    Adding components to App

---
+++ src/core/App.js
@@ -0,0 +1,15 @@
+import { Background } from "../components/Background";
+import { Header } from "../components/Header";
+import { SectionOne } from "../components/SectionOne";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Background />
+      <SectionOne />
+    </>
+  );
+}
+
+export default App;
