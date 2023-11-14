9c68b7f8c7cdc81be932f6df30643f9f770371fd
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-11-14 19:13:44 +0100

    create global theme

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    black: "#121212",
+    nero: "#3A3A3A",
+    sunglow: "#FFC32B",
+    orange: "#C23B21",
+    solitude: "#F2F3F5",
+    endeavour: "#2E5895",
+    sanMarino: "#254677",
+    white: "#fff",
+  },
+};
