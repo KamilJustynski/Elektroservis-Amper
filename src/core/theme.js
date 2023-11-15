6f4f7c90f299619ae9d4addd49311b223f311e71
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-11-15 21:11:53 +0100

    added new colors for theme

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,9 +1,12 @@
 export const theme = {
   colors: {
     black: "#121212",
+    blackNero: "#222222",
     nero: "#3A3A3A",
     sunglow: "#FFC32B",
+    kournikova: "#FFCF55",
     orange: "#C23B21",
+    burntUmber: "#9B2F1A",
     solitude: "#F2F3F5",
     endeavour: "#2E5895",
     sanMarino: "#254677",
