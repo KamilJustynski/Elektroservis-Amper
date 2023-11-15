c95a96f224ef714f981c291141a14056dd130957
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-11-15 21:12:21 +0100

    added new sections for the App

--- src/core/App.js
+++ src/core/App.js
@@ -1,6 +1,8 @@
 import { Background } from "../components/Background";
 import { Header } from "../components/Header";
 import { SectionOne } from "../components/SectionOne";
+import { SectionThree } from "../components/SectionThree";
+import { SectionTwo } from "../components/SectionTwo";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Header />
       <Background />
       <SectionOne />
+      <SectionTwo />
+      <SectionThree />
     </>
   );
 }
