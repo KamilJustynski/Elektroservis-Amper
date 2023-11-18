e3fb55684595c9e261192cc9d0b2f82fc0759d8a
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-11-18 12:53:13 +0100

    added new component SectionFour

---
+++ src/components/SectionFour/index.js
@@ -0,0 +1,50 @@
+import {
+  Container,
+  GridWrapper,
+  GridItem,
+  Title,
+  Description,
+} from "../SectionThree/styled";
+import { Wrapper, SectionTitle, Image, ImageWrapper } from "./styled";
+import Picture from "./image.png";
+
+export const SectionFour = () => (
+  <Container center>
+    <ImageWrapper>
+      <Image src={Picture} alt="Electrical box" />
+    </ImageWrapper>
+    <Wrapper>
+      <SectionTitle>Serwis elektryczny</SectionTitle>
+      <GridWrapper>
+        <GridItem>
+          <Title>Rezystancji izolacji kabli i przewodów</Title>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur. Libero a sem purus arcu.
+            Morbi ut leo vestibulum urna nec. Sed nulla volutpat posuere mauris.
+          </Description>
+        </GridItem>
+        <GridItem>
+          <Title>Skuteczności ochrony od porażeń</Title>
+          <Description>
+            Dolor etiam bibendum nam nulla tempus auctor amet. Penatibus in elit
+            neque bibendum habitant odio malesuada ullamcorper.
+          </Description>
+        </GridItem>
+        <GridItem>
+          <Title>Wyłączników RCD</Title>
+          <Description>
+            Nunc sit ac posuere sed aliquam ultrices mattis. At aliquet praesent
+            eu at lacus euismod praesent.
+          </Description>
+        </GridItem>
+        <GridItem>
+          <Title>Instalacji odgromowych</Title>
+          <Description>
+            Eget sed nunc quisque at tristique in urna faucibus tellus.
+            Condimentum sed a natoque bibendum.
+          </Description>
+        </GridItem>
+      </GridWrapper>
+    </Wrapper>
+  </Container>
+);
---
+++ src/components/SectionFour/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 458px;
+`;
+
+export const Image = styled.img`
+  width: 458px;
+  height: auto;
+  border-radius: 20px 0;
+  box-shadow: -7px 7px 26px -9px ${({ theme }) => theme.colors.black};
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const SectionTitle = styled.h1`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 48px;
+  font-weight: 400;
+  margin: 0;
+  margin-bottom: 72px;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 13%;
+    bottom: -10px;
+    width: 108px;
+    height: 2px;
+    background-color: ${({ theme }) => theme.colors.orange};
+    transform: translateX(-50%);
+  }
+`;
